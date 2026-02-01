150e8d8127fc90f0ced3b034d65719de1c9d7d3b
victor <victor.dramba@gmail.com>
2026-01-31 22:02:22 -0800

    Fix invite login flow in dev mode

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     port: 3102,
     proxy: {
-      '/api': process.env.API_URL || 'http://127.0.0.1:3101'
+      '/api': process.env.API_URL || 'http://127.0.0.1:3101',
+      '/invite': process.env.API_URL || 'http://127.0.0.1:3101'
     }
   }
 });
